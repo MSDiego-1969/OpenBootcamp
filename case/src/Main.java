58cfe3908488d45372c1e5bd071684c7d4eb71ed
Diego <mazuecosdiego@gmial.com>
2022-07-15 10:04:17 -0300

    Practico 3 java

---
+++ case/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "jjk";
+
+        switch (str){
+
+            case "Verano":
+                System.out.println("Verano");
+                break;
+
+            case "Otoño":
+                System.out.println("Otoño");
+                break;
+
+            case "Invierno":
+                System.out.println("Invierno");
+                break;
+
+            case "Primavera":
+                System.out.println("Primavera");
+                break;
+
+            default:
+                System.out.println("¿?");
+                break;
+        }
+
+    }
+}
---
+++ if_elseif_else/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int i= -1;
+        if (i>=0){
+            System.out.println("El número es positivo");
+        }
+        else {
+            System.out.println("El número es negativo");
+        }
+    }
+}
